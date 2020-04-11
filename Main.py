d1fa75b34cf63380210b91f366d3a348791cf12e
Nikita <nikhvatov19@gmail.com>
2020-04-11 19:00:51 +0300

    v2

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+from flask import Flask, render_template, request, redirect, url_for, make_response
 import vk_api
 import requests
 
@@ -10,6 +11,7 @@ from Info import Info
 from Reader import Reader
 from Writer import Writer
 
+
 class Main:
     """Конфигурация"""
 
@@ -19,7 +21,7 @@ class Main:
         self.Vk_token = Vk_token
 
 
-Config = Main('login','Password','Vk_token')
+Config = Main('89646997211','!vSNkei.KKcr1oE9mtT','ad02893880d0a9db76154d91da00a045e57494854dea997e90f3c534c1435fcec691d980d47191129c76b')
 
 session = requests.Session()
 vk_session = vk_api.VkApi(Config.Lonin, Config.Password)
@@ -29,59 +31,43 @@ vk_session = vk_api.VkApi(token=Config.Vk_token)
 
 vk = vk_session.get_api()
 
-menu = {
-1:'1: Перейти к разделам чата',
-2:'2: Получить информацию о аккаунте',
-3:'3: Войти в Online',
-4:'4: Войти в Offline',
-5:'5: Принимать сообщения',
-6:'6: Написать сообщение',
-}
-while True:
-    print('-------------------')
-    print('Выберете действие: ')
-    print(menu[1])
-    print(menu[2])
-    print(menu[3])
-    print(menu[4])
-    print(menu[5])
-    print(menu[6])
-    print('-------------------')
+app = Flask(__name__)
 
-    menuOption = int(input("Выбери действие: "))
-    if menuOption == 1:
-        try:
-            Chat(vk)
-        except:
-            print('-------------------')
-            print('Что то пошло не так')
-    if menuOption == 2:
-        try:
-            Info(vk)
-        except:
-            print('-------------------')
-            print('Что то пошло не так')
-    if menuOption == 3:
-        try:
-            EternalOnline(vk)
-        except:
-            print('-------------------')
-            print('Что то пошло не так')
-    if menuOption == 4:
-        try:
-            EternalOffline(vk)
-        except:
-            print('-------------------')
-            print('Что то пошло не так')
-    if menuOption == 5:
-        try:
-            Reader(vk,vk_session)
-        except:
-            print('-------------------')
-            print('Что то пошло не так')
-    if menuOption == 6:
-        try:
-            Writer(vk)
-        except:
-            print('-------------------')
-            print('Что то пошло не так')
+
+@app.route('/', methods=['GET'])
+def home():
+    print('ok')
+    response = make_response('ok', 200)
+    # response.headers['Access-Control-Allow-Origin'] = '*'
+    # return response
+    chats = vk.messages.getConversations()
+    print(chats['items'])
+
+    data = {'data': chats}
+
+    response = make_response(data, 200)
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
+
+@app.route('/<id>', methods=['GET'])
+def Chat(id=None):
+    print(id)
+    # response.headers['Access-Control-Allow-Origin'] = '*'
+    # return response
+    chats = vk.messages.getHistory(
+    user_id=id,
+    offset=50,
+    rev=0
+    )
+
+
+    list = {'data': chats}
+
+    print(list)
+
+    return render_template('index.html', chats=list['data']['items'])
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
--- writer.py
+++ writer.py
@@ -13,15 +13,15 @@ def Writer(vk):
     while True:
         data = input("Отправить сообщение: ")
 
-        try:
-            user_id = data.split(',')[0]
-            text = data.split(',')[1]
+        # try:
+        user_id = data.split(',')[0]
+        text = data.split(',')[1]
 
-            vk.messages.send( #Отправляем сообщение
-            user_id=user_id,
-            message=text,
-            random_id=get_random_id())
+        vk.messages.send( #Отправляем сообщение
+        user_id=user_id,
+        message=text,
+        random_id=get_random_id())
 
-            print('Сообщение отправленно.')
-        except:
-            print('Отправить не получилось.')
+        #     print('Сообщение отправленно.')
+        # except:
+        #     print('Отправить не получилось.')
